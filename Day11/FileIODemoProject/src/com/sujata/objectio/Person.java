9d506d83f88d3d47e1c29f90a6d719355567ed15
Sujatabatra <sujatabatra@hotmail.com>
2022-10-04 14:51:32 +0530

    Add files via upload

--- Day11/FileIODemoProject/src/com/sujata/objectio/Person.java
+++ Day11/FileIODemoProject/src/com/sujata/objectio/Person.java
@@ -13,9 +13,21 @@ import lombok.ToString;
 @ToString
 public class Person implements Serializable {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6555227171815680851L;
 	private int personId;
 	private String personName;
 	private int age;
+	private String address;
+	public Person(int personId, String personName, int age) {
+		super();
+		this.personId = personId;
+		this.personName = personName;
+		this.age = age;
+	}
+	
 	
 	
 }
